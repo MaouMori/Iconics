bb5e8a12478e82b246e202613b42dc76d5d7e958
Maou Mori <viniciusderticxd@gmail.com>
2026-03-18 00:18:34 -0300

    arrumando coisas2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
 import "./globals.css";
 
+export const metadata = {
+  title: "ICONICS",
+  description: "Fraternidade ICONICS",
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
